c7edb966bb5fa4915c087d3fbf950742b6659fee
Angel Barajas <Barajasb15@gmail.com>
2022-05-27 02:47:43 -0500

    add: string de conexion en la nube

--- PetCouple/Models/PetCoupleContext.cs
+++ PetCouple/Models/PetCoupleContext.cs
@@ -29,7 +29,7 @@ namespace PetCouple.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=Asura;Initial Catalog=PetCouple;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Server=tcp:petcoupleserver.database.windows.net,1433;Initial Catalog=PetCouple;Persist Security Info=False;User ID=PetCoupleAdmi;Password=PetCouplecontraseña1.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
